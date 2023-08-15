dcaa5b93bb56035011c2a146a41cf4e5522a7651
Deicy Rojas <deicy.rojas@ceinfes.com>
2023-08-15 15:52:33 -0500

    script insert

---
+++ Script_insert_bd_fundacion.sql
@@ -0,0 +1,9 @@
+INSERT INTO DB_FUNDACION.dbo.DocType (Type_document,alias,Active) VALUES 
+	('Cédula Ciudadanía',	'CC',	1),	
+	('NIT',	'NIT',	1	),
+	('Tarjeta Identidad',	'TI',	1),
+	('Cédula Extranjería',	'CEXT',	1),
+	('Pasaporte',	'PASS',	1	),
+	('Registro Civil',	'RC',	1),
+	('NUIP',	'NUIP',	1	),
+	('Permiso Especial de Permanencia (PEP)',	'PEP',	1);
